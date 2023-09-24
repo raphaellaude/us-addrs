6763300f38a882d908d72d47edd1341fe6ce8397
Raphael Paul Laude <raphlaude@gmail.com>
2023-09-24 12:21:59 -0400

    handle punctuation special cases

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,61 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate unicode_normalization;
 use std::collections::HashMap;
 
-lazy_static! {
-    pub static ref REPLACEMENTS: HashMap<&'static str, &'static str> = {
-        let mut m = HashMap::new();
-        m.insert("AVE", "AVENUE");
-        m.insert("ST", "STREET");
-        m
-    };
+// module imports
+mod abbreviations;
+use unicode_normalization::UnicodeNormalization;
+
+pub use crate::abbreviations::constants::STREET_SUFFIX_ABBREVS;
+
+pub fn remove_insignificant_punctuation(address: &str) -> String {
+    let included_nonalphanum: [char; 3] = ['-', '.', '/'];
+
+    let mut output = String::new();
+    let chars: Vec<char> = address.chars().collect();
+
+    for i in 0..chars.len() {
+        let c = chars[i];
+
+        if c.is_alphanumeric() || c.is_whitespace() {
+            output.push(c);
+            continue;
+        }
+
+        if i > 0 && i < chars.len() - 1 {
+            let prev = chars[i - 1];
+            let next = chars[i + 1];
+
+            // periods, hyphens, and slashes are significant and should not be removed
+            // except for hyphens in place names, which should be replaced with a space
+            let sig_alphanum = included_nonalphanum.contains(&c);
+
+            if prev.is_numeric() && next.is_numeric() && sig_alphanum {
+                output.push(c);
+                continue;
+            }
+
+            if sig_alphanum {
+                output.push(' ');
+                continue;
+            }
+        }
+    }
+    output
 }
 
 pub fn clean_address(address: &str, replacements: &HashMap<&str, &str>) -> String {
-    // periods, hyphens, and slashes are significant and should not be removed
-    // except for hyphens in place names, which should be replaced with a space
-    let included_nonalphanum: [char; 3] = ['-', '.', '/'];
-
-    let cleaned: String = address
+    let address: String = address
+        .trim()
         .chars()
-        .filter(|&c| c.is_alphanumeric() || c.is_whitespace() || included_nonalphanum.contains(&c))
+        .nfkd() // filter to ascii characters only, by closest
         .collect();
 
-    let uppercased = cleaned.to_uppercase();
+    let address: String = remove_insignificant_punctuation(&address);
+    let address: String = address.to_uppercase();
 
-    uppercased
+    address
         .split_whitespace()
         .map(|word| *replacements.get(word).unwrap_or(&word))
         .collect::<Vec<&str>>()
