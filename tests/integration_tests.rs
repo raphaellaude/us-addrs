46d707e7c0250c0b666c7bdacacb21d16c23d5d8
Raphael Paul Laude <raphlaude@gmail.com>
2023-09-24 10:01:05 -0700

    fix test imports

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,4 +1,4 @@
-use rust_addr_clean::{clean_address, STREET_SUFFIX_ABBREVS};
+use rust_addr_clean::{clean_address, SINGLE_WORD_ABBREVS};
 use std::fs::read_to_string;
 
 #[test]
@@ -6,7 +6,7 @@ fn test_clean_address() {
     let data = read_to_string("tests/test_addrs.txt").expect("Could not read file");
 
     for (i, line) in data.lines().enumerate() {
-        let cleaned = clean_address(line, &STREET_SUFFIX_ABBREVS);
+        let cleaned = clean_address(line, &SINGLE_WORD_ABBREVS);
         // Add assertions
         // all uppercase
         // no nonalphanum and spaces
