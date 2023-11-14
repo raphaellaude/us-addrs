d5109d37642319e5ea4a3fa7d9d6588093e953d3
Raphael Paul Laude <raphlaude@gmail.com>
2023-11-13 23:24:57 -0500

    test split on punc and spaces

--- tests/test_tokenize.rs
+++ tests/test_tokenize.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use us_addrs::tokenize;
 
 #[test]
@@ -19,4 +21,33 @@ fn test_tokenizing() {
 
     tokens = tokenize("box#1 abc st");
     assert_eq!(tokens, vec!["box", "#", "1", "abc", "st"]);
+}
+
+#[test]
+fn test_split_on_punc() {
+    // let mut tokens = tokenize("1 abc st,suite 1");
+    // assert_eq!(tokens, vec!["1", "abc", "st,", "suite", "1"]);
+
+    // tokens = tokenize("1 abc st;suite 1");
+    // assert_eq!(tokens, vec!["1", "abc", "st;", "suite", "1"]);
+
+    let tokens = tokenize("1-5 abc road");
+    assert_eq!(tokens, vec!["1-5", "abc", "road"]);
+}
+
+#[test]
+fn test_spaces() {
+    let result = vec!["1", "abc", "st"];
+
+    let mut tokens = tokenize("1 abc st");
+    assert_eq!(tokens, result);
+
+    tokens = tokenize("1 abc  st");
+    assert_eq!(tokens, result);
+
+    tokens = tokenize("1 abc st ");
+    assert_eq!(tokens, result);
+
+    tokens = tokenize(" 1 abc st");
+    assert_eq!(tokens, result);
 }
